ea3c1a3f7b2222c81d1d63093c3dc5e6ca346d12
darya <atomicleda@mail.ru>
2023-09-20 17:41:35 +0300

    login/singup

---
+++ src/routes/render/indexRouter.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import authCheck from '../../middlewares/authCheck';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const initState = { hello: 'world' };
+  res.render('Layout', initState);
+});
+
+
+router.get('/signup', authCheck(false), (req, res) => res.render('Layout'));
+
+router.get('/login', authCheck(false), (req, res) => res.render('Layout'));
+
+export default router;
