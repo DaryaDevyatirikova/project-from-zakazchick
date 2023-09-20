e8497d962fc0ab48adaaf8bc4e6e67fbb360227e
DaryaDevyatirikova <nady190277@yandex.ru>
2023-09-20 18:08:14 +0300

    Отрисовала домашнюю страницу

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import SortRecipe from '../ui/SortRecipe';
+import SortDESC from '../ui/icons/SortDESC';
+import SortABS from '../ui/icons/SortABS';
 
 export default function HomePage() {
   return (
     <>
-    HomePage
+      <div className="hstack gap-3">
+        <div className="p-2">
+          Сортировка по:
+          <SortRecipe />
+        </div>
+        <div className="p-2 ms-auto">
+          <button type="button">
+            <SortABS />
+          </button>
+        </div>
+        <div className="vr" />
+        <div className="p-2">
+          <button type="button">
+            <SortDESC />
+          </button>
+        </div>
+      </div>
+      <div className="card" style={{ width: '18rem' }}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the bulk of the card's
+            content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div>
-      <span>Кулинарная книга</span>
-      <a href="#">Избранное</a>
-      <a href="#">Войти</a>
-      <a href="#">Зарегистрироваться</a>
-      <a href="#">Выйти</a>
-    </div>
+    <ul className="nav nav-underline">
+      <li className="nav-item">Кулинарная книга</li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Избранное
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Войти
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Зарегистрироваться
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">
+          Выйти
+        </a>
+      </li>
+    </ul>
   );
 }
