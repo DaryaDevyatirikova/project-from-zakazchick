f146c5908bcd2d87b754db7d0cbb16f842e3431f
DaryaDevyatirikova <nady190277@yandex.ru>
2023-09-20 17:32:12 +0300

    Прописала базовую отрисовку домашней страницы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './ui/Navbar';
+import HomePage from './pages/HomePage';
+import RecipePage from './pages/RecipePage';
 
 export default function App({ hello }) {
-  return <div>Hello, {hello}</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/signup" element={<SignUpPage />} /> */}
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/:recipeId" element={<RecipePage />} />
+      </Routes>
+    </>
+  );
 }
---
+++ src/components/ui/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <div>
+      <span>Кулинарная книга</span>
+      <a href="#">Избранное</a>
+      <a href="#">Войти</a>
+      <a href="#">Зарегистрироваться</a>
+      <a href="#">Выйти</a>
+    </div>
+  );
+}
